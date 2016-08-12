53e4cb1539c727e94bfbdb658cac60ba5150345b
Jean Liu <jean.liu415@gmail.com>
2016-08-12 19:45:31 +0000

    got the chart in there and displaying data. Need to resize and add in second graph.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 """Adgnosco Server"""
 
 import arrow
+from collections import defaultdict
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, flash, redirect, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
@@ -176,112 +177,64 @@ def show_entries():
 def monthly_logs_data():
     """Return data about entries on a monthly basis"""
     # You will want to make this a get request in the future
+    # pass in the month so that you'll get the report for THAT month
     person_id = session['user_id']
     print person_id
 
+    ##### month = request.args.get('month')
+    month = 8
     entries = Entries.query.all()
-    buildings = Building.query.all()
 
-    print "******* ENTRIES *********"
-    print entries
-    print "******* BUILDING *********"
-    print buildings
+    # making nested dictionaries of the entries by door by day for the month specified
+    # dict_of_doors  = {entrance_id1:{day1:count1, day2:count2},
+    #                    entrance_id2:{day3:count3, day4:count4}
+    #                   }
+    dict_of_entries = {}
+    doors = Entrance.query.all()
+    print doors
+    for door in doors:
+        entriess = Entries.query.filter(Entries.entrance_id == door.entrance_id).all()
+        inner_dict = defaultdict(int)
+        for entry in entriess:
+            if entry.datentime.date().month == month:
+                day_of_month = entry.datentime.date().day
+                inner_dict[day_of_month] += 1
+        dict_of_entries[door.entrance_id] = inner_dict
+
+    list_color = ['#0001ff', '#59fa67', '#fff138',
+                  '#e62600', '#dd3cc4', '#b7cb34',
+                  '#ff6600', '#22dc09', '#af040c']
 
     # largest structure, the value is a list of dictionaries
-    monthly_dict = {'datasets': [] }
+    monthly_dict = {'datasets': []}
+
 
     # middle structure, making the large list of dictionaries
+    buildings = Building.query.all()
+    count = 0
     for building in buildings:
         building_name = building.building_name
         med_dict = {'label': building_name } 
-        med_dict['data'] = []
-        med_dict['backgroundColor'] = "#FF6384"
-        med_dict['hoverBackgoundColor'] = "#FF6380"
+        med_dict['data']=[]
+        med_dict['backgroundColor'] = list_color[count]
+        med_dict['hoverBackgoundColor'] = list_color[count]
         monthly_dict['datasets'].append( med_dict )
-        print monthly_dict
-
+        count += 1
 
         # smallest structure, making the list of mini dictionaries
         for entry in entries:
-            x_value = entry.datentime.date().day
-            y_value = entry.datentime.time().hour
-            r_value = 40
-            mini_dict = { 'x' : x_value, 'y' : y_value, 'r' : r_value}
-            med_dict['data'].append(mini_dict)
-            print med_dict
-
-
-
-    # monthly_dict = {
-    #     'datasets': [
-    #         {
-    #             'label': 'Cafeteria', (BUILDING NAME)
-    #             'data': [
-    #                 {
-    #                     'x': 1 (DATE)
-    #                     'y': 2 (HOUR)
-    #                     'r': (SUM OF PEOPLE *10)
-    #                 },
-    #                 {
-    #                     'x': 2,
-    #                     'y': 1,
-    #                     'r': 2
-    #                 }
-    #             ],
-    #             'backgroundColor': "#FF6384",
-    #             'hoverBackgoundColor': "#FF6380",
-    #         }]
-    # }
-    print monthly_dict
+            if entry.building_id == building.building_id:
+                x_value = entry.datentime.date().day
+                y_value = entry.entrance_id
+                # getting the r_value from the dict_of_entries above
+                days = dict_of_entries.get(y_value)
+                r_value = days.get(x_value) * 5
+                # back to making the mini dictionary
+                mini_dict = {'x':x_value, 'y':y_value, 'r':r_value}
+                med_dict['data'].append(mini_dict)
+        
     return jsonify(monthly_dict)
 
-
-
-
-
-
-
-
-
-
-
-
-
-    # new_user = User(email=email, password=password, age=age, zipcode=zipcode)
-
-    # db.session.add(new_user)
-    # db.session.commit()
-
-    # flash("User %s added." % email)
-    # return redirect("/users/%s" % new_user.user_id)
-
-
-
-# @app.route("/movies/<int:movie_id>", methods=['POST'])
-# def movie_detail_process(movie_id):
-#     """Add/edit a rating."""
-
-#     # Get form variables
-#     score = int(request.form["score"])
-
-#     user_id = session.get("user_id")
-#     if not user_id:
-#         raise Exception("No user logged in.")
-
-#     rating = Rating.query.filter_by(user_id=user_id, movie_id=movie_id).first()
-
-#     if rating:
-#         rating.score = score
-#         flash("Rating updated.")
-
-#     else:
-#         rating = Rating(user_id=user_id, movie_id=movie_id, score=score)
-#         flash("Rating added.")
-#         db.session.add(rating)
-
-#     db.session.commit()
-
-#     return redirect("/movies/%s" % movie_id)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
