9150f0ea72897bdd3fb1fc675ab94a85f0624b06
Jean Liu <jean.liu415@gmail.com>
2016-08-24 21:23:23 +0000

    we have 4 charts now on the entries page

--- server.py
+++ server.py
@@ -184,11 +184,29 @@ def show_entries():
                                            available_buildings=available_buildings)
 
 
+@app.route('/door.json')
+def door_data():
+    """Returns door options to populate dropdown"""
+    # get the building choosen from the page
+    building_name = request.args.get('building')
+    building_name = building_name.strip()
+    building_choosen = Building.query.filter(Building.building_name == building_name).first()
+    building_id = building_choosen.building_id
+    doors_of_choosen_building = Entrance.query.filter(Entrance.building_id == building_id).all()
+
+    # making a list of available doors in that building
+    doors_list = []
+    for each_door_in_building in doors_of_choosen_building:
+        door_choosen = each_door_in_building.entrance_id
+        doors_list.append(door_choosen)
+
+    # sends over that list of doors 
+    return jsonify({'doors_list':doors_list})
+
+
 @app.route('/monthly.json')
 def monthly_logs_data():
     """Return data about entries on a monthly basis"""
-    # You will want to make this a get request in the future
-    # pass in the month so that you'll get the report for THAT month
     person_id = session['user_id']
 
     year = 2016
@@ -252,31 +270,153 @@ def monthly_logs_data():
 
     return jsonify(monthly_dict)
 
-@app.route('/door.json')
-def door_data():
-    """Returns door options to populate dropdown"""
-    # get the building choosen from the page
+@app.route('/monthlyFR.json')
+def monthly_logs_FRdata():
+    """Return data about entries on a monthly basis caught by camera"""
+    person_id = session['user_id']
+
+    year = 2016
+    month = request.args.get('month')
+    print "Month = ", month
+    month = int(month)
+    
+
+    num_days = calendar.monthrange(year, month)[1]
+    start_date = datetime.date(year, month, 1)
+    end_date = datetime.date(year, month, num_days)
+
+    entries = (Entries.query.filter(Entries.datentime >= start_date,
+                                    Entries.datentime <= end_date).all())
+
+    # making nested dictionaries of the entries by door by day for the month specified
+    # dict_of_doors  = {entrance_id1:{day1:count1, day2:count2},
+    #                    entrance_id2:{day3:count3, day4:count4}
+    #                   }
+    dict_of_entries = {}
+    doors = Entrance.query.all()
+    for door in doors:
+        entriess = Entries.query.filter(Entries.entrance_id == door.entrance_id).all()
+        inner_dict = defaultdict(int)
+        for entry in entriess:
+            if entry.datentime.date().month == month:
+                day_of_month = entry.datentime.date().day
+                inner_dict[day_of_month] += 1
+        dict_of_entries[door.entrance_id] = inner_dict
+
+    list_color = ['#0001ff', '#59fa67', '#fff138',
+                  '#e62600', '#dd3cc4', '#b7cb34',
+                  '#ff6600', '#22dc09', '#af040c']
+
+    # largest structure, the value is a list of dictionaries
+    monthly_dict = {'datasets': []}
+
+
+    # middle structure, making the large list of dictionaries
+    buildings = Building.query.all()
+    count = 0
+    for building in buildings:
+        building_name = building.building_name
+        med_dict = {'label': building_name } 
+        med_dict['data']=[]
+        med_dict['backgroundColor'] = list_color[count]
+        med_dict['hoverBackgoundColor'] = list_color[count]
+        monthly_dict['datasets'].append( med_dict )
+        count += 1
+
+        # smallest structure, making the list of mini dictionaries
+        for entry in entries:
+            if entry.building_id == building.building_id:
+                x_value = entry.datentime.date().day
+                y_value = entry.entrance_id
+                # getting the r_value from the dict_of_entries above
+                days = dict_of_entries.get(y_value)
+                r_value = (days.get(x_value))*10
+                # back to making the mini dictionary
+                mini_dict = {'x':x_value, 'y':y_value, 'r':r_value}
+                med_dict['data'].append(mini_dict)
+
+    return jsonify(monthly_dict)
+
+@app.route('/building.json')
+def building_logs_data():
+    """Return data about entries into a building on a monthly basis """
+    person_id = session['user_id']
+
+    # getting all the variables set for the dictionaries creations later
+    year = 2016
+    month = request.args.get('month')
+    month = int(month)
+    #number of days in a month
+    num_days = calendar.monthrange(year, month)[1]
+    start_date = datetime.date(year, month, 1)
+    end_date = datetime.date(year, month, num_days)
+
     building_name = request.args.get('building')
     building_name = building_name.strip()
     building_choosen = Building.query.filter(Building.building_name == building_name).first()
     building_id = building_choosen.building_id
-    doors_of_choosen_building = Entrance.query.filter(Entrance.building_id == building_id).all()
 
-    # making a list of available doors in that building
-    doors_list = []
-    for each_door_in_building in doors_of_choosen_building:
-        door_choosen = each_door_in_building.entrance_id
-        doors_list.append(door_choosen)
+    door_choosen = request.args.get('door')
+    door_choosen = int(door_choosen)
 
-    # sends over that list of doors 
-    return jsonify({'doors_list':doors_list})
+    # making nested dictionaries of the entries by door by day for the month specified
+    # dict_of_doors  = {entrance_id1:{day1:{hour1a:count1b, hour1b:count1b}, day2:{hour2a:count2b, hour2b:count2b}},
+    #                   entrance_id2:{day1:{hour1a:count1b, hour1b:count1b}, day2:{hour2a:count2b, hour2b:count2b}}
+    #                   }
+    dict_of_entries = {}
+    byday_dict = {}
 
+    # hirarchy: dict_of_entries > entrace_dict > byday_dict > byhour_dict
+    # filtering all entries by the month and building specified
+    entriesbybuilding = (Entries.query.filter(Entries.building_id==building_id,
+                                              Entries.entrance_id==door_choosen,
+                                              Entries.datentime >= start_date,
+                                              Entries.datentime <= end_date).all())
 
-@app.route('/building.json')
-def building_logs_data():
-    """Return data about entries into a building on a monthly basis"""
-    # You will want to make this a get request in the future
-    # pass in the month so that you'll get the report for THAT month
+    # doorsinbuilding = Entrance.query.filter_by(building_id=building_id).all()
+
+    for i in range(num_days):
+        byhour_dict = defaultdict(int)
+        for entrybybuilding in entriesbybuilding:
+            if entrybybuilding.datentime.date().day == i:
+    # making a dictionary with count of entries
+                hourly = entrybybuilding.datentime.time().hour
+                byhour_dict[hourly] += 1
+                byday_dict[entrybybuilding.datentime.date().day] = byhour_dict
+            dict_of_entries[entrybybuilding.entrance_id] = byday_dict
+    
+    list_color = ['#0001ff', '#59fa67', '#fff138']
+
+    # largest structure, the value is a list of dictionaries
+    building_dict = {'datasets': []}
+
+    # middle structure, making the large list of dictionaries
+    count = 0
+    for door_id, door_info in dict_of_entries.items():
+        #entranceid = entry2.entrance_id
+        med_dict = {'label': 'Door id: %d' %(door_choosen)}
+        med_dict['backgroundColor'] = list_color[count]
+        med_dict['hoverBackgoundColor'] = list_color[count]
+        data = []
+
+        #build data
+        for day, hours in door_info.items():
+            x_value = day
+            for hour, people in hours.items():
+                y_value = hour
+                r_value = people
+                mini_dict = {'x':x_value, 'y':y_value, 'r':r_value}
+                data.append(mini_dict)
+
+        med_dict['data'] = data
+        building_dict['datasets'].append( med_dict )
+        count += 1
+
+    return jsonify(building_dict)
+
+@app.route('/buildingFR.json')
+def building_logs_FRdata():
+    """Return data about entries into a building on a monthly basis caught by camera"""
     person_id = session['user_id']
 
     # getting all the variables set for the dictionaries creations later
